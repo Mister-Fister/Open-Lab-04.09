5bc085d757a258f2c771c02bc8aeee52765d0007
Matúš <matusskor@gmail.com>
2020-10-30 14:52:16 +0100

    zmeny

--- Open-Lab-04.09/ArrayTools.cs
+++ Open-Lab-04.09/ArrayTools.cs
@@ -1,12 +1,20 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace Open_Lab_04._09
 {
     public class ArrayTools
     {
         public string[] RemoveDups(string[] strings)
         {
-            throw new NotImplementedException();
+            List<string> a = new List<string>();
+            for (int i = 0; i < strings.Length; i++)
+            {
+                if (!a.Contains(strings[i]))
+                {
+                    a.Add(strings[i]);
+                }
+            }
+            return a.ToArray();
         }
     }
 }
